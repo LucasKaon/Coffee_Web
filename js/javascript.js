a7b847320336f2723eeba78f16444f2ccabaf118
LucasKaon <amado.github@gmail.com>
2018-09-17 18:20:11 -0300

    Imagens editadas, menu criado, navegacao otimizada etc.

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,13 @@
 function Chamada(){
 	alert('Botão clicado.');
 }
+
+function click_Eat(){
+	document.getElementById('menu_eat').style.display = 'block';
+	document.getElementById('menu_drink').style.display = 'none';
+}
+
+function click_Drink(){
+	document.getElementById('menu_drink').style.display = 'block';
+	document.getElementById('menu_eat').style.display = 'none';
+}
